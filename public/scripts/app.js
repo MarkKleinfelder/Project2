58af3b144f1cf690c311264a251b163244e054e8
Mark <MarkKleinfelder@gmail.com>
2017-07-29 17:15:03 -0600

    adds js html css for UI/UX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,11 @@ $(document).ready(function(){
 
 
 
+		var angerLevel;
+		var disgustLevel;
+		var fearLevel;
+		var joyLevel;
+		var sadnessLevel;
 
 
 var submitText = function(){
@@ -12,10 +17,55 @@ var submitText = function(){
 		url:"https://watson-api-explorer.mybluemix.net/tone-analyzer/api/v3/tone?text="+$("#textToSubmit").val()+"&tones=emotion&sentences=false&version=2016-05-19",
 		type: 'Get',
 		success: function(data){
-		console.log(data);
+		console.log(data.document_tone.tone_categories[0].tones);
+		var tones=data.document_tone.tone_categories[0].tones;
+		angerLevel= tones[0].score * 100;
+		disgustLevel=tones[1].score *100;
+		fearLevel = tones[2].score *100;
+		joyLevel = tones[3].score *100;
+		sadnessLevel = tones[4].score *10;
+		graphResults();
 	}
+
   })
+	 
 }
+
+function graphResults() {
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      title:{
+        text: "Emotions"    
+      },
+      animationEnabled: true,
+      axisY: {
+        title: ""
+      },
+      legend: {
+        verticalAlign: "bottom",
+        horizontalAlign: "center"
+      },
+      theme: "theme2",
+      data: [
+
+      {        
+        type: "column",  
+        showInLegend: true, 
+        // legendMarkerColor: "grey",
+        // legendText: "MMbbl = one million barrels",
+        dataPoints: [      
+        {y: angerLevel, label: "Anger", color:"#e23852"},
+        {y: disgustLevel,  label: "Disgust", color:"#529b56"},
+        {y: fearLevel,  label: "Fear", color: "#f9e070"},
+        {y: joyLevel,  label: "Joy", color: "#ff9bf8"},
+        {y: sadnessLevel,  label: "Sadness", color: "#5d7ae2"}               
+        ]
+      }   
+      ]
+    });
+
+    chart.render();
+  }
 
 // var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 // var tone_analyzer = new ToneAnalyzerV3({

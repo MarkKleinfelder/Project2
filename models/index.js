50b6a9221ffcfe55b79281250a7db82a2c25e4c3
Mark <MarkKleinfelder@gmail.com>
2017-08-08 21:39:19 -0600

    heroku bugs. put mongoose back

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI); //|| 
-                  //process.env.MONGOLAB_URI || 
-                  //process.env.MONGOHQ_URL || 
-                  //"mongodb://localhost/project2");
+                  process.env.MONGOLAB_URI || 
+                  process.env.MONGOHQ_URL || 
+                  "mongodb://localhost/project2");
 
 
 

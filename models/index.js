865d65cbd50dca5e7a9bc928a2f0835f085f1280
Mark <MarkKleinfelder@gmail.com>
2017-08-08 21:45:11 -0600

    modified index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI); //|| 
+mongoose.connect( process.env.MONGODB_URI); || 
                   process.env.MONGOLAB_URI || 
                   process.env.MONGOHQ_URL || 
                   "mongodb://localhost/project2");

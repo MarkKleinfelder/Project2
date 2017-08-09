ba2e37e18e5f28e74238c7ae5719d8b2a11d8a51
Mark <MarkKleinfelder@gmail.com>
2017-08-08 21:47:29 -0600

    modifies index.js

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI); || 
                   process.env.MONGOLAB_URI || 
                   process.env.MONGOHQ_URL || 
-                  "mongodb://localhost/project2");
+                  "mongodb://localhost/project2";
 
 
 
